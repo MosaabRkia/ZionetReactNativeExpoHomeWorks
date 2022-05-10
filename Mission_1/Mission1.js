6429824af54429f00e0ef5f70ce4bc409a4f9a5d
Mosaab Abo Rkia <aborkiamosaab@gmail.com>
2022-05-10 14:42:52 +0300

    Done Mission 1

--- Mission_1/Mission1.js
+++ Mission_1/Mission1.js
@@ -1,5 +1,5 @@
-import { View, Text ,TextInput,Button} from 'react-native'
-import React, { useState } from 'react'
+import { View, Text ,TextInput,Button,ScrollView,TouchableOpacity} from 'react-native'
+import React, { useState ,useEffect} from 'react'
 import { windowHeight, windowWidth } from '../Utils/Dimensions'
 import { STATUSBAR_HEIGHT } from '../Utils/StatusHeight'
 import TitleCard from './componentsMission1/TitleCard'
@@ -8,21 +8,19 @@ import TitleCard from './componentsMission1/TitleCard'
 const Mission1 = () => {
     const [array,setArray] = useState([])
     const [title,setTitle] = useState("")
+    const [topItem,setTopItem] = useState(undefined)
    
+
     // Create Unique Id
-    const  UID =()=>{
-        let id;
-        do {
-            id = "id_" + Math.random().toString(16).slice(2)
-        } while (array.find(e=>e.id === id));
-        return id;
-    }
+    const  UID = "id_" + Math.random().toString(16).slice(2)
+
+    
     
     //add to array
     const addToTheArray = () =>{
         if(title.trim() !== '')
         {
-            setArray([...array,{id:UID,value:title}])
+            setArray([...array,{id:UID,value:title,marked:false,critical:false}])
             setTitle("")
             alert("Sucessfully Added !")
         }
@@ -37,10 +35,98 @@ const Mission1 = () => {
         alert("Sucessfully Deleted !")
     }
 
+    // mark from the array
+    const markFromTheArray=(id)=>{
+        setArray(array.map(item =>
+            item.id === id
+              ? {
+                  ...item,
+                  marked: !item.marked,
+                }
+              : item
+          ))
+        alert("Sucessfully Marked Changed!")
+        
+    }
+
+    // critical from the array
+    const criticalFromTheArray=(id)=>{
+        setArray(array.map(item =>
+            item.id === id
+              ? {
+                  ...item,
+                  critical: !item.critical,
+                }
+              : item
+          ))
+          alert("Sucessfully Critical Changed!")
+
+    }
+
+    // change title of item
+    const changeTitle=(id,newTitle)=>{
+        setArray(array.map(item =>
+            item.id === id
+              ? {
+                  ...item,
+                  value: newTitle,
+                }
+              : item
+          ))
+          alert("Sucessfully title Changed!")
+
+    }
+
+        // set top in the array
+        const setTop=(id)=>{
+            setTopItem(undefined)
+            array.length > 0 && array.forEach(item =>{
+                if(item.id === id)
+                    setTopItem(item)
+            }
+              )
+            
+              setArray(array.filter(item =>item.id !== id))
+              alert("Sucessfully added To Top!")
+    
+        }
+   
+
   return (
     <View style={{alignItems:'center'}}>
         {/* Header */}
-        <View style={{marginTop:STATUSBAR_HEIGHT,flexDirection:'row'}}>
+        <View style={{marginTop:STATUSBAR_HEIGHT}}>
+        
+        {/* top Item */}
+            <View >
+        {topItem === undefined?
+                <></>:
+                <View style={{borderBottomWidth:2}}>
+                <TitleCard 
+                item={topItem} 
+                removeFromTheArray={()=>setTopItem(undefined)} 
+                markFromTheArray={()=>setTopItem({...topItem,marked:!topItem.marked})}
+                criticalFromTheArray={()=>setTopItem({...topItem,critical:!topItem.critical})}
+                changeTitle={(id,text)=>setTopItem({...topItem,value:text})}
+                setTop={()=>alert("already in  top!")}
+                 />
+
+                <View style={{flexDirection: 'row',marginBottom:5,alignSelf:'center'}}>
+                        <Button
+                        title="Return To List"
+                        onPress={()=>{
+                            setArray([...array,topItem])
+                            setTopItem(undefined)
+                            alert("Sucessfully Returned To The List!")
+                        }}
+                        color={"blue"}
+                        />
+
+                    </View>
+                </View>   }
+            </View>
+        
+    <View style={{flexDirection:'row',marginTop:5}}>
     <TextInput
             placeholder='Enter title' 
                onChangeText={setTitle} 
@@ -48,25 +134,32 @@ const Mission1 = () => {
             keyboardType={"default"}
             style={{width:windowWidth * 0.8,borderWidth:1,textAlign:'center',height:windowHeight * 0.05}}   />
             <Button style={{borderWidth:1}} title ={"+"}
-            onPress={(e)=>{
+            onPress={()=>{
                 addToTheArray();
             }}
             />
         </View>
+        </View>
  
 
-
         {/* Body */}
-        <View>
-            {array.length>0 && array.map((e,index)=><TitleCard key={index} item={e} removeFromTheArray={removeFromTheArray} />)}
-            </View>
+        <ScrollView style={{width:windowWidth * 0.9,height:windowHeight*0.9,borderWidth:1,borderRadius:15,padding:5,marginTop:10}}>
+            {array.length>0 && array.map((e,index)=><TitleCard 
+            key={index} 
+            item={e} 
+            removeFromTheArray={removeFromTheArray} 
+            markFromTheArray={markFromTheArray}
+            criticalFromTheArray={criticalFromTheArray}
+            changeTitle={changeTitle}
+            setTop={setTop}
+            />)}
+        </ScrollView>
 
 
 
         {/* Footer */}
-        <View>
-            
-            </View>
+        <View> 
+ </View>
       
     </View>
   )
--- Mission_1/componentsMission1/TitleCard.js
+++ Mission_1/componentsMission1/TitleCard.js
@@ -1,13 +1,59 @@
-import { View, Text,Button } from 'react-native'
-import React from 'react'
-import { windowWidth } from '../../Utils/Dimensions'
+import { View, Text,Button,TextInput } from 'react-native'
+import React,{useState} from 'react'
+import { windowHeight, windowWidth } from '../../Utils/Dimensions'
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 const TitleCard = (props) => {
+  const [showEdit,setShowEdit] = useState(false)
+  const [newTitle,setNewTitle] = useState("")
+
+  const changeTitle=()=>{
+    if(newTitle === props.item.value)
+    alert("same title!")
+    else if(newTitle.trim() === '')
+    alert("empty title!")
+    else{
+      props.changeTitle(props.item.id,newTitle)
+      setNewTitle('')
+    }
+
+  }
   return (
-    <View style={{borderRadius:15,borderWidth:1,margin:5,width:windowWidth * 0.9,flexDirection:"row",justifyContent:'space-between'}}>
-      <Text style={{marginLeft:5,fontWeight:'bold',fontSize:25,textAlign:'center'}}>{props.item.value}</Text>
+    <View style={{backgroundColor:props.item.critical?"red":"white",borderRadius:15,borderWidth:props.item.marked?5:1,margin:5,width:windowWidth * 0.85,justifyContent:'space-around'}}>
+      <View>
+      <Text style={{fontWeight:'bold',fontSize:25,textAlign:'center'}}>{props.item.value}</Text>
+      </View>
+      <View style={{flexDirection:'row',alignSelf:'center'}}>
+
       <Icon name="remove" size={25} style={{width:windowWidth*0.1,alignSelf:'center'}} onPress={()=>props.removeFromTheArray(props.item.id)}/>
+      <Icon name="arrow-up" size={25} style={{width:windowWidth*0.1,alignSelf:'center'}} onPress={()=>props.setTop(props.item.id)}/>
+      <Icon name="edit" size={25} style={{width:windowWidth*0.1,alignSelf:'center'}} onPress={()=>setShowEdit(!showEdit)}/>
+           
+     <Icon name={props.item.marked?"bookmark":"bookmark-o"} size={25} style={{width:windowWidth*0.1,alignSelf:'center'}} onPress={()=>props.markFromTheArray(props.item.id)}/>
+      <Icon name={"exclamation-circle"} size={25} style={{width:windowWidth*0.1,alignSelf:'center'}} onPress={()=>props.criticalFromTheArray(props.item.id)}/>
+      </View>
+      {
+        showEdit?
+<View style={{flexDirection:'row',alignSelf:'center',margin:5}}>        
+        <TextInput
+        placeholder='Enter New title' 
+           onChangeText={setNewTitle} 
+           value={newTitle}
+        keyboardType={"default"}
+        style={{width:windowWidth * 0.7,borderWidth:1,textAlign:'center',height:windowHeight * 0.05,alignSelf:'center',borderLeftWidth:10}}   />
+  <View style={{justifyContent:'center',borderWidth:1,backgroundColor:'green',padding: 5}}>
+
+<Icon name="edit" style={{alignSelf:'center'}} size={25}
+            onPress={()=>{
+                changeTitle();
+            }}
+            />
+    </View>
+
+</View>
+        :<></>
+      }
+      
     </View>
   )
 }
